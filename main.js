060b24e00c748aeb5edbc83a49226390c19c936d
_ext Shelemekh, Kateryna <k.shelemekh.ext@dieboldnixdorf.com>
2020-11-09 11:29:07 +0200

    add logger

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
 let mainWindow;
+
+// log.transports.file.level = 'info';
+// log.transports.file.file = __dirname + 'log.log';
 
 function createWindow () {
   mainWindow = new BrowserWindow({
@@ -11,13 +15,14 @@ function createWindow () {
       nodeIntegration: true,
     },
   });
-  mainWindow.loadFile('index.html');
+
+  mainWindow.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
   mainWindow.once('ready-to-show', () => {
-    autoUpdater.logger = require("electron-log")
-    autoUpdater.logger.transports.file.level = "info"
+   
+    log.info('before trigger check updates ad notifications')
     autoUpdater.checkForUpdatesAndNotify();
   });
 }
@@ -43,16 +48,16 @@ ipcMain.on('app_version', (event) => {
 });
 
 autoUpdater.on('update-available', () => {
-  console.log("autoUpdater update method run");
+ log.info("autoUpdater update method run");
   mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
-  console.log("autoUpdater update_downloaded method run");
+ log.info("autoUpdater update_downloaded method run");
   mainWindow.webContents.send('update_downloaded');
 });
 
 ipcMain.on('restart_app', () => {
-  console.log("autoUpdater restart_app method run");
+ log.info("autoUpdater restart_app method run");
   autoUpdater.quitAndInstall();
 });
